70e7752c0fc526b392b3433596e8cae50ba86119
aidenjoo99 <aiden.joo.22@gmail.com>
2026-02-25 12:08:30 -0500

    syntax fixes

--- source/Player.h
+++ source/Player.h
@@ -27,13 +27,12 @@ private:
     /** The current health of the character and player*/
     int _currentHealth;
     /** The  ability class of the character*/
-    AbilityClass _abilityClass;
+    CharacterLoader::AbilityClass _abilityClass;
     /** The spritesheet correlating to this character*/
     std::string _spritesheetPath;
     /** The list of the characters special abilities*/
     std::vector<std::string> _specialAbilities;
-}
-
+    
 public:
     /**
      *Creates a player instance giiven a character ID
@@ -47,14 +46,14 @@ public:
      * Discards the player and releases all resources
      */
     Player() {};
-
+    
 #pragma mark Properties
     /**
      * Returns the inventory of the player
      */
-    const std::vector<ItemInstance> getInventory() const { return _inventory;}
-
-    /** 
+    std::vector<ItemInstance> getInventory() const { return _inventory; }
+    
+    /**
      * Returns whether player's inventory is not empty
      */
     bool hasItems() const { return !_inventory.empty(); }
@@ -62,48 +61,48 @@ public:
     /**
      * Returns the player number
      */
-    int getPlayerNumber const { return _playerNumber;}
-
+    int getPlayerNumber() const { return _playerNumber; }
+    
     /**
      * Returns the player name
      */
-    std::string getPlayerName const { return _playerName;}
-
+    std::string getPlayerName() const { return _playerName; }
+    
     /**
      * Return the name of the character
      */
-    std::string getCharacterName const {return _characterID;}
-
+    std::string getCharacterName() const { return _characterId; }
+    
     /**
      * Return the house of the character
      */
-    std::string getCharacterHouse const {return _house;}
-
+    std::string getCharacterHouse() const { return _house; }
+    
     /**
      * Return the max health of the character/player
      */
-    float getMaxHealth const {return _maxHealth;}
-
+    float getMaxHealth() const { return _maxHealth; }
+    
     /**
      * Return the current health of the character/player
      */
-    float getCurrentHealth const {return _currentHealth;}
-
+    float getCurrentHealth() const { return _currentHealth; }
+    
     /**
      * Returns the ability class of the character
      */
-    AbilityClass getAbilityClass const {return _abilityClass;}
+    CharacterLoader::AbilityClass getAbilityClass() const { return _abilityClass; }
     
     /**
      * Returns the path of the spritesheet for the character
      */
-    std::string getSpritesheetPath const {return _spritesheetPath;}
-
+    std::string getSpritesheetPath() const { return _spritesheetPath; }
+    
     /**
      * Returns the list of the characters special ability items
      */
-    std::vector<std::string> getSpecialAbilities const {return _specialAbilities;}
-
+    std::vector<std::string> getSpecialAbilities() const { return _specialAbilities; }
+    
 #pragma mark Gameplay
     /**
      * Updates the current health of the player by the given delta.
@@ -111,19 +110,19 @@ public:
      * Health is clamped between 0 and maxHealth.
      * @param delta     The amount to change health by
      */
-
+    
     void updateHealth(float delta);
     /**
      * Adds an item to the player's inventory.
      * @param item      The item to add
      */
     void addItem(const ItemInstance& item);
-
+    
     /**
      * Returns whether the player is alive or not
      */
     bool isAlive();
-
+    
     /**
      * Uses and removes an item from the player's inventory by item id.
      * @param item    The item to remove
@@ -132,5 +131,7 @@ public:
      */
     template <typename T>
     bool useItemById(ItemInstance::ItemId itemId, T& target);
-
+    
+};
+    
 #endif /* !__PLAYER_H__ */
