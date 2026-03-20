f10bb89a2cfccdbcf63da217a020045ddebb46da
Patrick Evuleocha <157386969+patrickce1@users.noreply.github.com>
2026-03-20 14:22:56 -0400

    Apply suggestion from @patrickce1

--- source/scenes/GameScene.cpp
+++ source/scenes/GameScene.cpp
@@ -399,7 +399,6 @@ std::shared_ptr<const ItemDef> GameScene::getHeldItemDef(ItemInstance::ItemId it
     }
     return nullptr;
 }
-    
 
 /**
  * Calls the appropriate handle action helper based on the input that we recieved
