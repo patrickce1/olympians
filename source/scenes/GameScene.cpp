45ccb2a9a67808cf08bbc77f95e6c559dd5a7fa0
aidenjoo99 <aiden.joo.22@gmail.com>
2026-03-21 14:21:43 -0400

    fix before main

--- source/scenes/GameScene.cpp
+++ source/scenes/GameScene.cpp
@@ -17,7 +17,7 @@ using namespace std;
 #define SCENE_HEIGHT 852
 
 namespace {
-constexpr float ITEM_PHYSICS_UNITS = 1.0f;
+    constexpr float ITEM_PHYSICS_UNITS = 1.0f;
 }
 
 #pragma mark -
@@ -155,7 +155,7 @@ bool GameScene::init(const std::shared_ptr<cugl::AssetManager>& assets, const st
     we recheck if we are player 0 whenever another scene transitions back into this one*/
     setLocalPlayer(0);
     
-    setDebugMode(true);
+    setDebugMode(false);
     setActive(false);
     return true;
 }
