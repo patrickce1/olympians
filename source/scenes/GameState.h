fac6acf6d81a977467c3c06232ff4595d5d64b8a
Zion Singleton <82426376+SnowiiHDR@users.noreply.github.com>
2026-03-24 17:32:33 -0400

    Update source/scenes/GameState.h
    Co-authored-by: Danielle Imogu <dannieim689@gmail.com>

--- source/scenes/GameState.h
+++ source/scenes/GameState.h
@@ -196,7 +196,6 @@ public:
      */
     std::shared_ptr<Enemy> getEnemy() const { return _enemy; }
 
-
 #pragma mark - Game State Checking
     /* Returns whether or not the players won based on the current game state*/
     bool checkWon();
