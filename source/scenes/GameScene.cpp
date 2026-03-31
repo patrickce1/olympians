6cda4f7964177a68a444b4fd88aaac95b32160c3
Danielle Imogu <dannieim689@gmail.com>
2026-03-31 14:44:55 -0400

    change to parent node

--- source/scenes/GameScene.cpp
+++ source/scenes/GameScene.cpp
@@ -114,7 +114,7 @@ bool GameScene::initSceneGraph() {
                _assets->get<scene2::SceneNode>("gameScene.gameArea.enemyHealth.label"));
         
         // This is the boss animation sprite, you can change the texture and set frames as needed.
-        _bossSprite = std::dynamic_pointer_cast<scene2::SpriteNode>((_gameArea->getChildByName("bossAnimationSpace")));
+        _bossSprite = std::dynamic_pointer_cast<scene2::SceneNode>((_gameArea->getChildByName("bossAnimationSpace")));
         
         // This is the special effects node, this is where all the animated effects will go.
         _specialEffectsLayer = scene2::SceneNode::allocWithBounds(dimen);
