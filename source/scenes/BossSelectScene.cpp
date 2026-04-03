7dde5639c98599deaa42a429d7f98aa1ffea950c
Danielle Imogu <dannieim689@gmail.com>
2026-04-03 12:47:05 -0400

    cpp done

--- source/scenes/BossSelectScene.cpp
+++ source/scenes/BossSelectScene.cpp
@@ -1,1 +1,289 @@
+#include "BossSelectScene.h"
+
+using namespace cugl;
+using namespace cugl::netcode;
+using namespace std;
+
+#pragma mark -
+#pragma mark Level Layout
+
+/** Regardless of logo, lock the height to this */
+#define SCENE_HEIGHT  852
+/** Role card width */
+#define ROLE_CARD_WIDTH 251
+/** Interpolation smoothing factor*/
+#define SMOOTHING_FACTOR 0.2f
+
+
+#pragma mark -
+#pragma mark Provided Methods
+/**
+ * Initializes the controller contents, and starts the game
+ *
+ * In previous labs, this method "started" the scene.  But in this
+ * case, we only use to initialize the scene user interface.  We
+ * do not activate the user interface yet, as an active user
+ * interface will still receive input EVEN WHEN IT IS HIDDEN.
+ *
+ * That is why we have the method {@link #setActive}.
+ *
+ * @param assets    The (loaded) assets for this game mode
+ *
+ * @return true if the controller is initialized properly, false otherwise.
+ */
+bool BossSelectScene::init(const std::shared_ptr<cugl::AssetManager>& assets, const std::shared_ptr<NetworkController>& networkController) {
+    // Initialize the scene to a locked width
+    if (assets == nullptr) {
+        return false;
+    } else if (!Scene2::initWithHint(Size(0,SCENE_HEIGHT))) {
+        return false;
+    }
+    
+    // Start up asset manager, network controller, and enemy loader
+    _assets = assets;
+    _network = networkController;
+    loadBosses();
+    
+    Size dimen = getSize();
+    
+    // Acquire the scene built by the asset loader and resize it the scene
+    std::shared_ptr<scene2::SceneNode> scene = _assets->get<scene2::SceneNode>("bossSelectScene");
+    scene->setContentSize(dimen);
+    scene->doLayout(); // Repositions the HUD
+
+    setupUI();
+    setupListeners();
+    
+    _status = Status::WAIT;
+    
+    addChild(scene);
+    setActive(false);
+    return true;
+}
+
+/**
+ * Retrieves and stores references to the BossSelectScene UI elements.
+ *
+ * This method looks up UI components from the scene graph including the
+ * lock button, back button, carousel navigation
+ * buttons, and the role carousel container. It also initializes the
+ * carousel item list.
+ */
+void BossSelectScene::setupUI() {
+
+    _backOut = std::dynamic_pointer_cast<scene2::Button>(
+        _assets->get<scene2::SceneNode>("bossSelectScene.back"));
+    
+    _lockButton = std::dynamic_pointer_cast<scene2::Button>(
+        _assets->get<scene2::SceneNode>("bossSelectScene.lock"));
+
+    _leftButton = std::dynamic_pointer_cast<scene2::Button>(
+        _assets->get<scene2::SceneNode>("bossSelectScene.bossCarousel.directionButtons.leftScroll"));
+
+    _rightButton = std::dynamic_pointer_cast<scene2::Button>(
+        _assets->get<scene2::SceneNode>("bossSelectScene.bossCarousel.directionButtons.rightScroll"));
+
+    _bossSelectionCardContainer = _assets->get<scene2::SceneNode>("bossSelectScene.bossCarousel.bossCardContainer");
+
+    if (_bossSelectionCardContainer) {
+        for (int i = 0; i < 3; i++) {
+            _bossCards.push_back(_bossSelectionCardContainer->getChild(i));
+        }
+    }
+    
+    auto bossCarouselDotsContainer = _assets->get<scene2::SceneNode>("bossSelectScene.bossSelectionCarouselIcons");
+    
+    if (bossCarouselDotsContainer) {
+        for (int i = 0; i < 3; i++) {
+            _bossCarouselDotIndicators.push_back(bossCarouselDotsContainer->getChild(i));
+        }
+    }
+}
+
+/**
+ * Attaches input listeners to the boss select buttons.
+ *
+ * This method assigns callbacks for starting the game, returning to the
+ * previous menu, and navigating the role selection carousel.
+ */
+void BossSelectScene::setupListeners() {
+    
+    _backOut->addListener([this](const std::string& name, bool down) {
+        if (down) {
+            _status = Status::ABORT;
+        }
+    });
+    
+    _lockButton->addListener([this](const std::string& name, bool down) {
+        if (down) {
+            _status = Status::LOCK;
+        }
+    });
+
+    _leftButton->addListener([this](const std::string& name, bool down){
+        if (!down) slideTo(_currentIndex - 1);
+    });
+
+    _rightButton->addListener([this](const std::string& name, bool down){
+        if (!down) slideTo(_currentIndex + 1);
+    });
+}
+
+/**
+ * Disposes of all (non-static) resources allocated to this mode.
+ */
+void BossSelectScene::dispose() {
+    if (_active) {
+        removeAllChildren();
+        _backOut = nullptr;
+        _lockButton = nullptr;
+        _bossCards.clear();
+        _leftButton = nullptr;
+        _rightButton = nullptr;
+        _bossSelectionCardContainer = nullptr;
+        _active = false;
+    }
+    _network = nullptr;
+}
+
+/**
+ * Sets whether the scene is currently active
+ *
+ * This method should be used to toggle all the UI elements.  Buttons
+ * should be activated when it is made active and deactivated when
+ * it is not.
+ *
+ * @param value whether the scene is currently active
+ */
+void BossSelectScene::setActive(bool value) {
+    if (isActive() != value) {
+        Scene2::setActive(value);
+        if (value) {
+            _status = WAIT;
+            _leftButton->activate();
+            _rightButton->activate();
+            _backOut->activate();
+            _lockButton->activate();
+        } else {
+            _leftButton->deactivate();
+            _rightButton->deactivate();
+            _backOut->deactivate();
+            _lockButton->deactivate();
+            
+            // If any were pressed, reset them
+            _backOut->setDown(false);
+            _leftButton->setDown(false);
+            _rightButton->setDown(false);
+            _lockButton->setDown(false);
+        }
+    }
+}
+
+/**
+ * The method called to update the scene.
+ *
+ * We need to update this method to constantly talk to the server
+ *
+ * @param timestep  The amount of time (in seconds) since the last frame
+ */
+void BossSelectScene::update(float timestep) {
+    if (_isAnimating) {
+        Vec2 current = _bossSelectionCardContainer->getPosition();
+        Vec2 next = current.lerp(_slideTarget, SMOOTHING_FACTOR); // 0.2 = smoothing factor
+
+        if (current.distance(_slideTarget) < 1.0f) {
+            _bossSelectionCardContainer->setPosition(_slideTarget);
+            _isAnimating = false;
+        } else {
+            _bossSelectionCardContainer->setPosition(next);
+        }
+    }
+}
+
+/**
+ * Reconfigures the lock button for this scene
+ *
+ * This is necessary because what the buttons do depends on the state of the
+ * networking.
+ */
+void BossSelectScene::configureLockButton() {
+    if (_network->isHost()) {
+        _lockButton->activate();
+        _lockButton->setVisible(true);
+    } else {
+        _lockButton->deactivate();
+        _lockButton->setVisible(false);
+    }
+}
+
+/**
+ * Initiates a slide animation to center the item at `newIndex`.
+ *
+ * Does nothing if an animation is already in progress or if the
+ * index is out of bounds. Otherwise computes the target container
+ * position and stores it in `_slideTarget`.
+ *
+ * @param newIndex The index of the item to slide to.
+ */
+void BossSelectScene::slideTo(int newIndex) {
+    if (_isAnimating) return;
+    if (newIndex < 0 || newIndex >= _bossCards.size()) return;
+
+    _isAnimating = true;
+
+    float shiftAmount = ROLE_CARD_WIDTH;
+    
+    int deltaIndex = newIndex - _currentIndex;
+    Vec2 currentPos = _bossSelectionCardContainer->getPosition();
+    float targetX = currentPos.x - (deltaIndex * shiftAmount);
+    
+    _slideTarget = Vec2(targetX, currentPos.y);
+    _currentIndex = newIndex;
+    
+    // Set the visibility of all glow overlays to false and the currentIndex card's to true
+    for (int i = 0; i < _bossCards.size(); i++) {
+        auto card = _bossCards[i];
+        if (card) {
+            auto glow = card->getChildByName("glowOverlay");
+            if (glow){
+                glow->setVisible(false);
+                if (i == newIndex) {
+                    glow->setVisible(true);
+                }
+            }
+        }
+    }
+
+    updateCarouselDots(newIndex);
+}
+
+/**
+ * Updates the circular indicators at the bottom of what card in the carousel
+ * we are currently at.
+ *
+ * @param currentIndex The index of the card we are at.
+ */
+void BossSelectScene::updateCarouselDots(int currentIndex) {
+    for (int i = 0; i < _bossCarouselDotIndicators.size(); i++) {
+        auto node = _bossCarouselDotIndicators[i];
+        
+        auto fill   = node->getChildByName("fill");
+        
+        if (i == currentIndex) {
+            fill->setColor(Color4("#4c3214ff"));
+        } else {
+            fill->setColor(Color4("#9d7137ff"));
+        }
+    }
+}
+
+/** Loads boss definitions from the enemies JSON to use in selection. */
+bool BossSelectScene::loadBosses() {
+    const std::string enemiesJsonPath = "json/enemies.json";
+    if (!_enemyLoader.loadFromFile(enemiesJsonPath)) {
+        CULog("HostSetupScene: Failed to load enemies.json");
+        return false;
+    }
+    return true;
+}
 
