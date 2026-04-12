71f03f52d8fc3fe27162502148a1dae178b7aef1
Danielle Imogu <dannieim689@gmail.com>
2026-04-12 02:18:01 -0400

    dim out lobby indicator

--- source/scenes/LobbyScene.cpp
+++ source/scenes/LobbyScene.cpp
@@ -406,10 +406,15 @@ void LobbyScene::update(float timestep) {
         if (_blinkTimer >= BLINK_TIMER) {
             _blinkTimer = 0.0f;
             _blinkOn = !_blinkOn;
-            _localPlayerIconIndicator->setVisible(_blinkOn);
+            if(_blinkOn){
+                _localPlayerIconIndicator->setColor(Color4(255,255,255,255));
+            } else {
+                _localPlayerIconIndicator->setColor(Color4(255,255,255,150));
+            }
         }
     } else {
         _localPlayerIconIndicator->setVisible(true);
+        _localPlayerIconIndicator->setColor(Color4(255,255,255,255));
     }
 }
 
