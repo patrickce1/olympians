ac81622beef407992201ac26e6b5b8a1dfa69e91
aidenjoo99 <aiden.joo.22@gmail.com>
2026-04-19 19:18:24 -0400

    added item calc log

--- source/Player.cpp
+++ source/Player.cpp
@@ -185,6 +185,8 @@ static float computeResolvedItemMagnitude(const Player& player,
     if (resolvedMagnitude <= 0.0f) {
         resolvedMagnitude = 0.0f;
     }
+    
+    CULog("[Item Value Calculation]\nItem: '%s'\nEffective value: '%.3f'",
           def.getName().c_str(),
           resolvedMagnitude);
 
