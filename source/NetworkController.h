5618d1071037a84001ed30887aed5a60746d5aab
aidenjoo99 <aiden.joo.22@gmail.com>
2026-04-23 18:40:45 -0400

    network controller spacing docs fixes

--- source/NetworkController.h
+++ source/NetworkController.h
@@ -59,11 +59,12 @@ public:
     */
     bool init(const std::shared_ptr<cugl::AssetManager>& assets);
 
-    /*Returns the current state of the connection. Check the Status enum for possible values*/
+    /** Returns the current state of the connection. Check the Status enum for possible values */
     Status checkConnection();
 
-    /*Tells the network controller to 
-    Calling this function will populate the message queues and variables with new information*/
+    /** Tells the network controller to
+     *  Calling this function will populate the message queues and variables with new information
+     */
     void getNetworkUpdates();
 
     /*Clears all message queues. 
@@ -72,20 +73,21 @@ public:
     void clearQueues();
 
     /**
-    * Connects to the game server as specified in the assets file
-    *
-    * The {@link #init} method set the configuration data. This method simply uses
-    * this to create a new {@Link NetworkConnection}. 
-    *
-    * @param room  The room ID to use. Should be a 5-digit base 10 number
-    *
-    * To check the status of the connection, use {@link #checkConnection()}
-    */
+     * Connects to the game server as specified in the assets file
+     *
+     * The {@link #init} method set the configuration data. This method simply uses
+     * this to create a new {@Link NetworkConnection}.
+     *
+     * @param room  The room ID to use. Should be a 5-digit base 10 number
+     *
+     * To check the status of the connection, use {@link #checkConnection()}
+     */
     void joinRoom(const std::string room);
 
-    /*Creates a lobby for other players to join, 
-    IMPORTANT: the program could be still negotiating connection
-    by the end of the function. Use {@link #checkConnection()} to ensure status of connection*/
+    /** Creates a lobby for other players to join,
+     * IMPORTANT: the program could be still negotiating connection
+     * by the end of the function. Use {@link #checkConnection()} to ensure status of connection
+     */
     void hostRoom();
 
     /*Returns a string with the room id if the room exists. Nullptr otherwise*/
@@ -130,6 +132,7 @@ public:
      * @param playerID   The 0-based index of the player receiving the effect.
      */
     void broadcastSupportEffect(SupportEffectType effectType, float magnitude, float duration, int playerID);
+    
     /**
      * Sends an enemy-affecting attack effect to the host for authoritative processing.
      *
@@ -364,24 +367,24 @@ protected:
         ENEMY_EFFECT = 15
     };
 
-    /*Our network connection*/
+    /** Our network connection */
     std::shared_ptr<cugl::netcode::NetcodeConnection> _network;
 
-    /*Serializer and desializer that lets us make bytes more readable and easy to decode across the network*/
+    /** Serializer and desializer that lets us make bytes more readable and easy to decode across the network */
     cugl::netcode::NetcodeSerializer _serializer;
     cugl::netcode::NetcodeDeserializer _deserializer;
 
-    /*Keeps track of the id of the game we are in*/
+    /** Keeps track of the id of the game we are in */
     std::string _gameid;
 
-    /*Keeps track of the connection status*/
+    /** Keeps track of the connection status */
     Status _status;
 
     /** The network configuration */
     cugl::netcode::NetcodeConfig _config;
     
 private:
-    /* Lists that keep track of the updates sent by players to the host */
+    /** Lists that keep track of the updates sent by players to the host */
     std::vector<AttackMessage> attacks;
     std::vector<PassMessage> passes;
     std::vector<HealMessage> heals;
