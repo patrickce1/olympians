40122f3a958d103d0725b69fe84fc68a0dab5ce1
Mikael Andreasyan <mikaelandreasyan@gmail.com>
2026-04-23 23:47:33 -0400

    final stuff

--- source/Enemy.h
+++ source/Enemy.h
@@ -35,7 +35,7 @@ public:
 
 protected:
     /** Debug boolean. Set to false to prevent debug statements */
-    bool _debug = false;
+    bool _debug = true;
 
     /** Unique identifier for this enemy (e.g., "cyclops", "cerberus") */
     std::string _enemyId;
