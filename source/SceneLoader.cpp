340797c2d7161df9fb69893d21a6ae076108eaf1
Zion Singleton <zs394@cornell.edu>
2026-04-27 14:12:51 -0400

    Update SceneLoader.cpp

--- source/SceneLoader.cpp
+++ source/SceneLoader.cpp
@@ -1,6 +1,5 @@
 //Implementation of SceneLoader
 #include "SceneLoader.h"
-#include "FirebaseBridge.h"
 
 // This keeps us from having to write cugl:: all the time
 using namespace cugl;
@@ -55,8 +54,6 @@ CU_ROOTCLASS(SceneLoader)
  * causing the application to run.
  */
 void SceneLoader::onStartup() {
-    initFirebase();
-
     // Create a sprite batch (and background color) to render the scene
     _batch = SpriteBatch::alloc();
     setClearColor(Color4("#1e1410ff"));
