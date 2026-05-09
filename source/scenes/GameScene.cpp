31edafd71508f58a63582758a46999db07714e67
Danielle Imogu <dannieim689@gmail.com>
2026-05-09 14:05:35 -0400

    second zone adjustment

--- source/scenes/GameScene.cpp
+++ source/scenes/GameScene.cpp
@@ -539,8 +539,8 @@ void GameScene::initInputZones(){
     _attackArea->setVisible(false);
     
     _supportZones = {
-        {InputController::Action::DROP_ALLY_LEFT,  Rect(-w * 0.149f, h * 0.37f, w * 0.399f, h * 0.55f)},
-        {InputController::Action::DROP_ALLY_RIGHT, Rect(w * 0.75f,   h * 0.37f, w * 0.399f, h * 0.55f)},
+        {InputController::Action::DROP_ALLY_LEFT,  Rect(-w * 0.149f, h * 0.37f, w * 0.36f, h * 0.55f)},
+        {InputController::Action::DROP_ALLY_RIGHT, Rect(w * 0.79f,   h * 0.37f, w * 0.399f, h * 0.55f)},
     };
       
     _inventoryZones = {
