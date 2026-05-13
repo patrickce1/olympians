7ebec8a7e5e94d004381035d5ee3875aaff3cfb6
Mikael Andreasyan <72567838+mikael-andreasyan@users.noreply.github.com>
2026-05-13 16:05:41 -0400

    Update source/scenes/GameScene.h
    Co-authored-by: Patrick Evuleocha <157386969+patrickce1@users.noreply.github.com>

--- source/scenes/GameScene.h
+++ source/scenes/GameScene.h
@@ -175,7 +175,6 @@ struct GaiaVineAnimation {
 
     int frameCount = 12;
     int currentFrame = -1;
-
     float duration = 0.5f;     // default value for now, should match the build up time
     float currentTime = 0.0f;  // keeps track of how long we've been in the state for
 
