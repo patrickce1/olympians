901ddad84e2f60e44cf8a9b79015603e1805a3ad
Mikael Andreasyan <72567838+mikael-andreasyan@users.noreply.github.com>
2026-05-12 23:37:33 -0400

    Update source/NetworkController.cpp
    Co-authored-by: Danielle Imogu <dannieim689@gmail.com>

--- source/NetworkController.cpp
+++ source/NetworkController.cpp
@@ -1254,6 +1254,7 @@ void NetworkController::broadcastPlayerScramble(const std::array<int, 4>& mappin
 * are reassigned.
 * Safe to call on both host and clients when handling a
 * MID_GAME_SCRAMBLE message.
+* 
 * @param newMapping represents the new order, where newMapping[i] is the new slot that
 *        player i ended up in. For example, if newMapping[0] = 1, that means that the player
 *        at slot 0 ended up at slot 1 after the scramble
