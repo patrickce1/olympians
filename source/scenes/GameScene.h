891a96a5007e1ca2d18bd7f55567ca7830a83b03
Mikael Andreasyan <72567838+mikael-andreasyan@users.noreply.github.com>
2026-05-13 16:06:16 -0400

    Update source/scenes/GameScene.h
    Co-authored-by: Patrick Evuleocha <157386969+patrickce1@users.noreply.github.com>

--- source/scenes/GameScene.h
+++ source/scenes/GameScene.h
@@ -172,7 +172,6 @@ struct AnimationEntry {
 struct GaiaVineAnimation {
     std::shared_ptr<cugl::scene2::SpriteNode> leftNode;
     std::shared_ptr<cugl::scene2::SpriteNode> rightNode;
-
     int frameCount = 12;
     int currentFrame = -1;
     float duration = 0.5f;     // default value for now, should match the build up time
