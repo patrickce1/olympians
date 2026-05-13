49408036aa5615da79e7436f4d70bf3514274a36
Mikael Andreasyan <72567838+mikael-andreasyan@users.noreply.github.com>
2026-05-12 23:37:41 -0400

    Update source/NetworkController.cpp
    Co-authored-by: aidenjoo99 <132617916+aidenjoo99@users.noreply.github.com>

--- source/NetworkController.cpp
+++ source/NetworkController.cpp
@@ -1229,7 +1229,7 @@ void NetworkController::swapSlots(int slotA, int slotB) {
 * player i ended up in. For example, if newMapping[0] = 1, that means that the player
 * at slot 0 ended up at slot 1 after the scramble
 */
-void NetworkController::broadcastPlayerScramble(const std::array<int, 4>& mapping) {
+void NetworkController::broadcastPlayerScramble(const std::array<int, 4>& newMapping) {
     if (!_network->isHost()) {
         return;
     }
